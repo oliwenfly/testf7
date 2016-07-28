d2863336c5d919694ba31a7eada2d16b7feb8d55
oliwen <oliwen@126.com>
2016-07-28 19:35:30 +0800

    no message

--- src/js/weixintest.js
+++ src/js/weixintest.js
@@ -3,6 +3,43 @@
  */
 var WeixinSdk = function() {};
 
-WeixinSdk.ss = function() {
-    alert('aaaa');
+/**
+ * 初始化
+ */
+WeixinSdk.init = function() {
+};
+
+/**
+ * 微信配置成功调用
+ */
+wx.ready(function() {
+    myApp.alert("微信配置成功!");
+});
+
+/**
+ * 微信配置失败调用
+ */
+wx.error(function(res){
+    myApp.alert('微信配置失败!');
+});
+
+/**
+ * 检测微信的接口
+ */
+WeixinSdk.check = function() {
+    wx.checkJsApi({
+        jsApiList: [
+            'getNetworkType',
+            'previewImage'
+        ],
+        success: function (res) {
+            myApp.alert(JSON.stringify(res));
+        },
+        fail: function(err) {
+            myApp.alert(JSON.stringify(err));
+        },
+        complete: function (res) {
+            myApp.alert(JSON.stringify(res));
+        }
+    });
 }
